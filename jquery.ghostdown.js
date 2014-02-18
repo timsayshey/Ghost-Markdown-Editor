43f957298fb53e7554d052763b99dd220047859e
lucor <lu.corbo@gmail.com>
2014-02-18 15:10:45 +0100

    Refactored jquery.ghostdown.js as widget plugin

--- jquery.ghostdown.js
+++ jquery.ghostdown.js
@@ -1,54 +1,71 @@
 (function ($, ShowDown, CodeMirror) {
     "use strict";
 
-    $.fn.ghostDown = function() {
-
-        var self = this;
-
-        var converter = new ShowDown.converter();
-
-        var editor = CodeMirror.fromTextArea(this.find('textarea')[0], {
+    $.widget( "b4m.ghostDown", {
+        editor: null,
+        markdown: null,
+        html: null,
+        converter: null,
+        _create: function() {
+            this.converter = new ShowDown.converter();
+            this.editor = CodeMirror.fromTextArea(this.element.find('textarea')[0], {
                 mode: 'markdown',
                 tabMode: 'indent',
                 lineWrapping: true
             });
 
-        // Really not the best way to do things as it includes Markdown formatting along with words
-        function updateWordCount() {
-            var wordCount = self.find('.entry-word-count'),
-                editorValue = editor.getValue();
+            this.editor.on("change", $.proxy(function () {
+                this._updatePreview();
+            }, this));
 
-            if (editorValue.length) {
-                wordCount.html(editorValue.match(/\S+/g).length + ' words');
-            }
-        }
+            $('.entry-markdown header, .entry-preview header', this.element).click(function (e) {
+                $('.entry-markdown, .entry-preview', this.element).removeClass('active');
+                $(e.target, this.element).closest('section').addClass('active');
+            });
 
-        function updatePreview() {
-            var preview = self.find('.rendered-markdown');
-            preview.html(converter.makeHtml(editor.getValue()));
-            updateWordCount();
-        }
+            $('.CodeMirror-scroll', this.element).on('scroll', $.proxy(function (e) {
+                this._syncScroll(e);
+            }, this));
 
-        $('.entry-markdown header, .entry-preview header', self).click(function (e) {
-            $('.entry-markdown, .entry-preview', self).removeClass('active');
-            $(e.target, self).closest('section').addClass('active');
-        });
+            // Shadow on Markdown if scrolled
+            $('.CodeMirror-scroll', this.element).scroll(function(e) {
+                if ($(e.target).scrollTop() > 10) {
+                    $('.entry-markdown', this.element).addClass('scrolling');
+                } else {
+                    $('.entry-markdown', this.element).removeClass('scrolling');
+                }
+            });
+            // Shadow on Preview if scrolled
+            $('.entry-preview-content', this.element).scroll(function(e) {
+                if ($('.entry-preview-content', $(e.target).scrollTop()).scrollTop() > 10) {
+                    $('.entry-preview', this.element).addClass('scrolling');
+                } else {
+                    $('.entry-preview', this.element).removeClass('scrolling');
+                }
+            });
 
-        editor.on("change", function () {
-            updatePreview();
-        });
-
-        updatePreview();
-
-        // Sync scrolling
-        function syncScroll(e) {
+            this._updatePreview();
+        },
+        _updatePreview: function() {
+            var preview = this.element.find('.rendered-markdown');
+            this.markdown = this.editor.getValue();
+            this.html = this.converter.makeHtml(this.markdown);
+            preview.html(this.html);
+            this._updateWordCount();
+        },
+        getHtml: function () {
+            return this.html;
+        },
+        getMarkdown: function () {
+            return this.markdown;
+        },
+        _syncScroll: function (e) {
             // vars
-            var $codeViewport = $(e.target, self),
-                $previewViewport = $('.entry-preview-content', self),
-                $codeContent = $('.CodeMirror-sizer', self),
-                $previewContent = $('.rendered-markdown', self),
-
-            // calc position
+            var $codeViewport = $(e.target),
+                $previewViewport = $('.entry-preview-content'),
+                $codeContent = $('.CodeMirror-sizer'),
+                $previewContent = $('.rendered-markdown'),
+                // calc position
                 codeHeight = $codeContent.height() - $codeViewport.height(),
                 previewHeight = $previewContent.height() - $previewViewport.height(),
                 ratio = previewHeight / codeHeight,
@@ -56,36 +73,13 @@
 
             // apply new scroll
             $previewViewport.scrollTop(previewPostition);
+        },
+        _updateWordCount: function() {
+            var wordCount = this.element.find('.entry-word-count'),
+            editorValue = this.markdown;
+            if (editorValue.length) {
+                wordCount.html(editorValue.match(/\S+/g).length + ' words');
+            }
         }
-
-        // TODO: Debounce
-        $('.CodeMirror-scroll', self).on('scroll', syncScroll);
-
-        // Shadow on Markdown if scrolled
-        $('.CodeMirror-scroll', self).scroll(function() {
-            if ($('.CodeMirror-scroll', self).scrollTop() > 10) {
-                $('.entry-markdown', self).addClass('scrolling');
-            } else {
-                $('.entry-markdown', self).removeClass('scrolling');
-            }
-        });
-        // Shadow on Preview if scrolled
-        $('.entry-preview-content', self).scroll(function() {
-            if ($('.entry-preview-content', self).scrollTop() > 10) {
-                $('.entry-preview', self).addClass('scrolling');
-            } else {
-                $('.entry-preview', self).removeClass('scrolling');
-            }
-        });
-
-        $.fn.ghostDown.getMarkdown = function() {
-            return editor.getValue();
-        };
-
-        $.fn.ghostDown.getHtml = function() {
-            return $('.rendered-markdown', self).html();
-        };
-
-        return this;
-    };
+    });
 }(jQuery, Showdown, CodeMirror));
